a4e0dfecbc6f2c8cb2779c6f74c2cd0b25294296
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-04-27 08:19:48 +0600

    add

---
+++ JavaClass2/src/javaclass2/JavaClass2.java
@@ -0,0 +1,24 @@
+package javaclass2;
+
+import java.util.Scanner;
+
+public class JavaClass2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the value of Radius:");
+        int radius = input.nextInt();
+        double area;
+
+        area = radius * radius * 3.1416;
+        System.out.println(area);
+
+//        int radius;
+//        double area;
+//        radius = 20;
+//        area = radius * radius * 3.1416;
+//        System.out.println(area);
+    }
+
+}
