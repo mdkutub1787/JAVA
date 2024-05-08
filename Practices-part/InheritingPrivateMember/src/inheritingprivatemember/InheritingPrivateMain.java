23c9e8f9a75d02405dd771165310e3b4daf6aae8
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-08 12:42:59 +0600

    add

--- Practices-part/InheritingPrivateMember/src/inheritingprivatemember/InheritingPrivateMain.java
+++ Practices-part/InheritingPrivateMember/src/inheritingprivatemember/InheritingPrivateMain.java
@@ -7,21 +7,21 @@ public class InheritingPrivateMain {
 
     public static void main(String[] args) {
 
-//        Apple a = new Apple();
-//        a.setMakeText("Massage");
-//        a.setCamera("50 Px");
-//        a.setVideo("Mx Player");
-//        a.setPlayStore("Game");
-//        a.setAppStore("apple app");
-//        a.displayInformation();
-//
-//        Apple a1 = new Apple();
-//        a1.setMakeText("Massanger");
-//        a1.setCamera("100 Px");
-//        a1.setVideo("Q Player");
-//        a1.setPlayStore("Free Fire ");
-//        a1.setAppStore("apple app2");
-//        a1.displayInformation();
+        Apple a = new Apple();
+        a.setMakeText("Massage");
+        a.setCamera("50 Px");
+        a.setVideo("Mx Player");
+        a.setPlayStore("Game");
+        a.setAppStore("apple app");
+        a.displayInformation();
+
+        Apple a1 = new Apple();
+        a1.setMakeText("Massanger");
+        a1.setCamera("100 Px");
+        a1.setVideo("Q Player");
+        a1.setPlayStore("Free Fire ");
+        a1.setAppStore("apple app2");
+        a1.displayInformation();
 
         Student s = new Student();
         s.setName("Kutub Uddin");
