3824a1c5e218c339667ddfee4403cab230cc038f
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-09 10:22:00 +0600

    add

--- Class-14 Ex Handeling/ExceptionHandeling/src/testfile/TestFile.java
+++ Class-14 Ex Handeling/ExceptionHandeling/src/testfile/TestFile.java
@@ -6,13 +6,14 @@ package testfile;
 import static java.awt.PageAttributes.MediaType.C;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Scanner;
 
 public class TestFile {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
         File file = new File("C:\\Users\\MD KUTUB UDDIN\\Desktop\\kutub.txt");
         System.out.println(file.exists());
@@ -29,6 +30,35 @@ public class TestFile {
 
         Scanner input = new Scanner(file);
         System.out.println(input.nextLine());
+
+//        File file = new File("C:\\Users\\MD KUTUB UDDIN\\Desktop\\kutub.txt");
+//        System.out.println("Exists: " + file.exists());
+//        System.out.println("Readable: " + file.canRead());
+//        System.out.println("Writable: " + file.canWrite());
+//        System.out.println("Is Dir: " + file.isDirectory());
+//        System.out.println("Is File: " + file.isFile());
+//        System.out.println("Absolute: " + file.isAbsolute());
+//        System.out.println("Hidden: " + file.isHidden());
+//
+//        System.out.println("Abs Path: " + file.getAbsolutePath());
+//        System.out.println("Canonical Path: " + file.getCanonicalPath());
+//
+//        System.out.println("Name: " + file.getName());
+//        System.out.println("Path: " + file.getPath());
+//        System.out.println("Parent: " + file.getParent());
+//        System.out.println("Last Modified: " + new Date(file.lastModified()));
+//        System.out.println("Length: " + file.length());
+//
+//        File dir = new File("C:\\Users\\MD KUTUB UDDIN\\Desktop\\kutub.txt");
+//        File[] files = dir.listFiles();
+//        System.out.println("Existing Files:\n");
+//        for (File f : files) {
+//            System.out.println("\t" + f.getName() + "\n");
+//        }
+//
+//        File testFile = new File("C:\\Users\\MD KUTUB UDDIN\\Desktop\\kutub.txt");
+//        System.out.println("File Created: " + testFile.createNewFile());
+
     }
 
 }
