6d1be1de81f84634e78bd01eb42971913afe7b7b
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-15 22:17:23 +0600

    add

--- Bank Mangment Project/BankMangment/src/bankmangmentproject/BankManagement.java
+++ Bank Mangment Project/BankMangment/src/bankmangmentproject/BankManagement.java
@@ -2,33 +2,12 @@ package bankmangmentproject;
 
 import java.util.Scanner;
 
-class BankAccount {
-    private double balance;
-
-    public BankAccount(double initialBalance) {
-        balance = initialBalance;
-    }
-
-    public void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposited: Tk " + amount);
-    }
-
-    public void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            System.out.println("Withdrawn: Tk " + amount);
-        } else {
-            System.out.println("Insufficient funds");
-        }
-    }
-
-    public void checkBalance() {
-        System.out.println("Current Balance: Tk " + balance);
-    }
-}
+/**
+ * @author MD KUTUB UDDIN
+ */
 
 public class BankManagement {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter initial balance: Tk ");
