fc5ec335b7ed43de0ebd8da3cb015a4f899c6e42
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-22 19:05:00 +0600

    add

--- OOPExercise/src/OOPExercise5/OOPExerciseTest5.java
+++ OOPExercise/src/OOPExercise5/OOPExerciseTest5.java
@@ -1,6 +1,5 @@
 package OOPExercise5;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -10,13 +9,14 @@ public class OOPExerciseTest5 {
 
     public static void main(String[] args) {
 
-        LinkedList<Book> books=new LinkedList<>();
-        
-        Book b=new Book("java", "python", 12332);
-        Book b1=new Book("java Towhid", "python", 12332);
-        
+        LinkedList<Book> books = new LinkedList<>();
+
+        Book b = new Book("java", "python", 12332);
+        Book b1 = new Book("java Towhid", "python", "ISBN : " +12332);
+
         books.add(b);
         books.add(b1);
+
         System.out.println(books);
     }
 
