bf938d96fb8f86e5eac6da14fa3bdc5355bbe9fa
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-23 20:24:24 +0600

    add

---
+++ BasicExercises/src/basicexercises/BasicExercises.java
@@ -0,0 +1,19 @@
+package basicexercises;
+
+/**
+ * @author MD KUTUB UDDIN
+ */
+public class BasicExercises {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nMd Kutub udin");
+
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+
+    }
+
+}
