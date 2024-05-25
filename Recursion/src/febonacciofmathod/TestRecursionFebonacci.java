23e895a160838d6e6563923a8867fdae3937a62d
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-25 21:09:51 +0600

    add

--- Recursion/src/febonacciofmathod/TestRecursionFebonacci.java
+++ Recursion/src/febonacciofmathod/TestRecursionFebonacci.java
@@ -1,14 +1,19 @@
 package febonacciofmathod;
 
+import java.util.Scanner;
+
 /**
  * @author MD KUTUB UDDIN
  */
 public class TestRecursionFebonacci {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter The  Number : ");
+        long f = scanner.nextInt();
 
         SubFebonacci feb = new SubFebonacci();
-        System.out.println("Febonacci is : " + feb.febonacci(11));
+        System.out.println("Febonacci of " + f + "  is : " + feb.febonacci(f));
 
     }
 }
