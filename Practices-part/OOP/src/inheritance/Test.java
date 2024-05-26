d7d7b56765d5a5f7b196fd3955d70bc41fef99f0
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-26 09:10:52 +0600

    add

--- Practices-part/OOP/src/inheritance/Test.java
+++ Practices-part/OOP/src/inheritance/Test.java
@@ -7,8 +7,8 @@ public class Test {
 
     public static void main(String[] args) {
 
- Teacher t=new Teacher(qualification, name, mail, 0)
-        
+        Teacher t = new Teacher("Python", "Towhid", "towhid123@gmail.com", 25);
+        System.out.println(t);
        
     }
 }
