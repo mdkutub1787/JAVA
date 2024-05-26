2eec6d3f977204ef5e202a6675b494c54d9d7f1e
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-26 10:18:35 +0600

    add

---
+++ Practices-part/OOPExercise/src/EncapsulatiionExercise/EncapsulatiionExerciseTest.java
@@ -0,0 +1,13 @@
+package EncapsulatiionExercise;
+
+/**
+ * @author MD KUTUB UDDIN
+ */
+public class EncapsulatiionExerciseTest {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee(124512, "Sanaullah", 50000);
+        System.out.println(emp);
+    }
+}
