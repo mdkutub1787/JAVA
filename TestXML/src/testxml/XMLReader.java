4bf978ebf88f9345b4a35dcb9771042fe93d29c6
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-06-02 19:04:07 +0600

    add

--- TestXML/src/testxml/XMLReader.java
+++ TestXML/src/testxml/XMLReader.java
@@ -1,6 +1,5 @@
 package testxml;
 
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
@@ -8,15 +7,14 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
 public class XMLReader {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = dbf.newDocumentBuilder();
-            Document d = builder.parse("student");
+            Document d = builder.parse("students.xml");
             d.getDocumentElement().normalize();
             Element root = d.getDocumentElement();
             System.out.println("Root Element : " + root.getNodeName());
@@ -33,18 +31,15 @@ public class XMLReader {
                     String position = element.getElementsByTagName("position").item(0).getTextContent();
                     String salary = element.getElementsByTagName("salary").item(0).getTextContent();
 
-                    System.out.println("Employee ID: " + id);
+                    System.out.println("Student ID: " + id);
                     System.out.println("Name: " + name);
                     System.out.println("Position: " + position);
                     System.out.println("Salary: " + salary);
                     System.out.println("---------------------------");
                 }
-
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
