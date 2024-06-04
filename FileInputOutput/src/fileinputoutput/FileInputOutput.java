4dc79ae9969be7ee977d1c23dcde4bc46b732af6
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-06-04 10:40:34 +0600

    adddddddddd

---
+++ FileInputOutput/src/fileinputoutput/FileInputOutput.java
@@ -0,0 +1,19 @@
+
+package fileinputoutput;
+
+import java.io.FileNotFoundException;
+
+
+public class FileInputOutput {
+
+   
+    public static void main(String[] args) throws FileNotFoundException {
+       
+       SubClass sc=new SubClass();
+        sc.getWrite();
+        sc.getRead();
+       
+        
+    }
+    
+}
