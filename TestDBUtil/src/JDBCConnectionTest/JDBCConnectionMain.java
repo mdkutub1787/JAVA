b678c6466419a1f201fe959d08874d5b8380294b
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-06-06 17:48:54 +0600

    add

--- TestDBUtil/src/JDBCConnectionTest/JDBCConnectionMain.java
+++ TestDBUtil/src/JDBCConnectionTest/JDBCConnectionMain.java
@@ -1,8 +1,11 @@
 package JDBCConnectionTest;
 
+import entity.Student;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,19 +13,33 @@ public class JDBCConnectionMain {
 
     static PreparedStatement ps;
     static ResultSet rs;
+    static JDBCConnection db = new JDBCConnection();
 
     public static void main(String[] args) {
+        Student student = new Student();
+        List<Student> studentList = getById(1);
 
-        JDBCConnection db = new JDBCConnection();
-        String insertSql = "insert into student(name, email, address, cell) "
+        student.setName(studentList.get(0).getName());
+        student.setEmail("Rajujee123456@gmail.com");
+        student.setAddress(studentList.get(0).getAddress());
+        student.setCell(studentList.get(0).getCell());
+        student.setId(studentList.get(0).getId());
+
+        editData(student);
+        showData();
+
+    }
+
+    public static void saveData() {
+        String insertSql = "insert into test2(name, email, address, cell) "
                 + "values(?,?,?,?)";
 
         try {
             ps = db.getCon().prepareStatement(insertSql);
             ps.setString(1, "Raju");
-            ps.setString(2, "Raju121@gmail.com");
+            ps.setString(2, "Raju@gmail.com");
             ps.setString(3, "Azimpur, Dhaka");
-            ps.setString(4, "0157522151");
+            ps.setString(4, "01151");
 
             ps.executeUpdate();
             ps.close();
@@ -31,8 +48,10 @@ public class JDBCConnectionMain {
         } catch (SQLException ex) {
             Logger.getLogger(JDBCConnectionMain.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
 
-        String selectSql = "select * from student";
+    public static void showData() {
+        String selectSql = "select * from test2";
 
         try {
             ps = db.getCon().prepareStatement(selectSql);
@@ -55,6 +74,58 @@ public class JDBCConnectionMain {
         } catch (SQLException ex) {
             Logger.getLogger(JDBCConnectionMain.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+    }
+
+    public static void editData(Student s) {
+
+        String sql = "update test2 set name=?, email=?, address=?, cell=? where id=?";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getEmail());
+            ps.setString(3, s.getAddress());
+            ps.setString(4, s.getCell());
+            ps.setInt(5, s.getId());
+
+            ps.executeUpdate();
+
+            ps.close();
+            db.getCon().close();
+            System.out.println("Edit Successfull");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnectionMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static List<Student> getById(int id) {
+         List<Student> studentList = new ArrayList<>();
+        String sql = "select * from test2 where id=?";
+
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Student s = new Student(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("address"),
+                        rs.getString("cell")
+                );
+                studentList.add(s);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnectionMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return studentList;
+       
 
     }
 
--- TestDBUtil/test/util/DbUtil.java
+++ TestDBUtil/test/util/DbUtil.java
@@ -1,4 +1,5 @@
-package JDBCConnectionTest;
+
+package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +7,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class JDBCConnection {
+
+public class DbUtil {
 
     private Connection con = null;
     private String url = "jdbc:mysql://localhost:3306/jdbctest";
@@ -15,15 +17,17 @@ public class JDBCConnection {
     private String driver = "com.mysql.cj.jdbc.Driver";
 
     public Connection getCon() {
+
         try {
             Class.forName(driver);
-             con = DriverManager.getConnection(url, user, password);
-        } catch (ClassNotFoundException |SQLException ex) {
-            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+            con = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
         return con;
-       
-
     }
 
 }
